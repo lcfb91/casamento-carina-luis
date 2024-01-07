58bd6a74771d4d75203760589a87dbdaf04449f5
Luis Borges <luis.borges@Luiss-MacBook-Pro.local>
2024-01-07 15:32:35 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Casamento Carina e Luis`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
