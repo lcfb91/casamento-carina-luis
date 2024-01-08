be6c6dd1858b6b700f0e2ba85fd2f3656d5166a2
Luis Borges <luis.borges1991@gmail.com>
2024-01-08 21:39:17 +0000

    Fix path

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,6 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  pathPrefix: "/casamento-carina-luis",
   siteMetadata: {
     title: `Casamento Carina e Luis`,
     //siteUrl: `https://www.yourdomain.tld`,
