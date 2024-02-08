ef83aea282cfe80c91aa4978049896d421a734ee
Luis Borges <luis.borges1991@gmail.com>
2024-02-08 18:55:26 +0000

    Change text

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,17 +6,6 @@ module.exports = {
     title: `Casamento Carina e Luis`,
     siteUrl: `https://carinaeluis.pt`,
   },
-  headers: [
-    {
-      source: `*`,
-      headers: [
-        {
-          key: `Access-Control-Allow-Origin`,
-          value: `carinaeluis.pt`,
-        },
-      ],
-    },
-  ],
   plugins: [
     {
       resolve: `gatsby-plugin-google-fonts`,
